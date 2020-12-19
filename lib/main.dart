fd1b4eff9752ad914c6882f30a085b2daf4f2e1c
Divyesh Bhatt <divyesh.bhatt23@gmail.com>
2020-12-19 12:35:28 +0530

    chap2 completed
    Basics of widgets done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     title: "Cool App",
     home: HomePage(),
   ));
 }
-    
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,30 @@ class HomePage extends StatelessWidget {
         title: Text("Cool App"),
       ),
       body: Container(
-        child: Center(child: Text("Hello Flutter!")),
+        color: Colors.indigoAccent,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepOrange,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepPurple,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.redAccent,
+            ),
+          ],
+        ),
       ),
     );
   }
